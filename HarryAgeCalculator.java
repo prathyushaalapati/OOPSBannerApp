902774657c8b711097371538fa8700f89441a986
pratyushaalapati <na0752@srmist.edu.in>
2026-02-23 09:48:13 +0530

    add HarryAgeCalculator file

---
+++ HarryAgeCalculator.java
@@ -0,0 +1,9 @@
+public class HarryAgeCalculator {
+
+    public static void main(String[] args) {
+        int birthYear = 2008;
+        int currentYear = 2024;
+        int harryAge = currentYear - birthYear;
+        System.out.println("Harry's age in " + currentYear + " is " + harryAge);
+    }
+}
