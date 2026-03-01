5c424bd081d2d000f2b984ee1cf4601c6c375ca4
pratyushaalapati <na0752@srmist.edu.in>
2026-03-01 13:52:07 +0530

    UC6 - Static helper methods implementation

---
+++ UC6.java
@@ -0,0 +1,40 @@
+public class UC6 {
+    public static String[] buildO() {
+        return new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        };
+    }
+    public static String[] buildP() {
+        return new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    "
+        };
+    }
+    public static String[] buildS() {
+        return new String[]{
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                "**** "
+        };
+    }
+    public static void main(String[] args) {
+        String[] o = buildO();
+        String[] p = buildP();
+        String[] s = buildS();
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + "  " +
+                               o[i] + "  " +
+                               p[i] + "  " +
+                               s[i]);
+        }
+    }
+}
